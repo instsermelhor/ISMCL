7d188f0092528321acbcbbe11e459b5b098eae97
Instituto Ser Melhor <admism@institutosermelhor.org>
2026-08-09 18:57:32 -0300

    Update event-bus.module.ts

--- backend/src/events/event-bus.module.ts
+++ backend/src/events/event-bus.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EventBusService } from './event-bus.service';
+import { RealtimeController } from './realtime.controller';
 
 @Module({
+  controllers: [RealtimeController],
   providers: [EventBusService],
   exports: [EventBusService],
 })
